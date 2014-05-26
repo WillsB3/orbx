0220596ca09ca177b2214bfaec3b435d6f51decb
Wills Bithrey <wills@potatolondon.com>
2014-05-26 20:38:28 +0100

    Add some more thumbnails to make it look a bit less same-y

--- assets/images/products/scraper.sh
+++ assets/images/products/scraper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for i in {45..47}
+for i in {1..28}
 do
    printf "Fetching image $i\n"
-   curl -O -L "http://fullterrain.com/images/ss/nanca/$i.jpg"
+   curl -O -L "http://fullterrain.com/images/ss/egpb/$i.jpg"
    printf "\n\n"
 done
